8c4415293eb4e6538ebf256ad545e84e059b182e
RaajuPahlowan <rajupahlowan@gmail.com>
2023-04-06 23:43:29 +0600

    tasksix

---
+++ LabTaskSix/addProduct.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Product</title>
+</head>
+<body>
+    <fieldset>
+    <legend>ADD PRODUCT</legend>
+        <form method="post" action="logCheck.php">
+            Name: <input type="text" name="username"/><br/>
+            Buying Price: <input type="password" name="password"/><br/>
+            Selling Price: <input type="password" name="password"/><br/>
+            <hr>
+            <input type= "checkbox" name="display" />Display
+            <hr>
+            <input type="submit" name="submit" value="Submit"/>
+        </form>
+    </fieldset>
+</body>
+</html>
